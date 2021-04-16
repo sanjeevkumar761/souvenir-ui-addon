69b5ac24949764db4c808be699430dc86ee29653
Sanjeev Kumar <sanjeku@microsoft.com>
2021-04-16 15:15:07 +0200

    Initial file

---
+++ install_terraform.sh
@@ -0,0 +1,4 @@
+wget https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_linux_amd64.zip
+unzip terraform_0.14.10_linux_amd64.zip
+mv terraform /usr/local/bin/
+export PATH=$PATH:/usr/local/bin/
