b17b6abc22104ca80f69acb02e157eb6d317fb83
Sanjeev Kumar <sanjeku@microsoft.com>
2021-04-16 15:54:58 +0200

    Added library setup steps

--- install_library.sh
+++ install_library.sh
@@ -1,6 +1,6 @@
-cd ~/Azure_SAP_Automated_Deployment
+cd ./Azure_SAP_Automated_Deployment
 cp sap-hana/documentation/SAP_Automation_on_Azure/Process_Documentation/WORKSPACES WORKSPACES/ -r
-cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
+cd ./Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
 
 #parameterize for North Europe / West Europe / Region specific setup
 cd LANDSCAPE
@@ -13,8 +13,8 @@ cp MGMT-WEEU-DEP00-INFRASTRUCTURE/ MGMT-NOEU-DEP00-INFRASTRUCTURE/ -r
 cd MGMT-NOEU-DEP00-INFRASTRUCTURE
 cp ./config_files/MGMT-NOEU-DEP00-INFRASTRUCTURE.json .
 
-rm ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/MGMT-NOEU-SAP_LIBRARY.json
+rm ./Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/MGMT-NOEU-SAP_LIBRARY.json
 cp ./config_files/MGMT-NOEU-SAP_LIBRARY.json ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
 
-cd ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
+cd ./Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
 ${DEPLOYMENT_REPO_PATH}deploy/scripts/install_library.sh -p MGMT-NOEU-SAP_LIBRARY.json -d ../../DEPLOYER/MGMT-NOEU-DEP00-INFRASTRUCTURE
