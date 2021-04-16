058acf08fc14d9a23253e3d8731dc58c50831bf6
Sanjeev Kumar <sanjeku@microsoft.com>
2021-04-16 17:56:20 +0200

    Fixed issues with path of json files

--- install_library.sh
+++ install_library.sh
@@ -1,20 +1,22 @@
 cd ~/Azure_SAP_Automated_Deployment
 cp sap-hana/documentation/SAP_Automation_on_Azure/Process_Documentation/WORKSPACES WORKSPACES/ -r
-cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
+
 
 #parameterize for North Europe / West Europe / Region specific setup
+cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
 cd LANDSCAPE
 cp DEV-WEEU-SAP01-INFRASTRUCTURE/ DEV-NOEU-SAP01-INFRASTRUCTURE/ -r
 cd DEV-NOEU-SAP01-INFRASTRUCTURE
-cp ./config_files/DEV-NOEU-SAP01-INFRASTRUCTURE.json .
+cp ~/souvenir-ui-addon/config_files/DEV-NOEU-SAP01-INFRASTRUCTURE.json .
 
+cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
 cd DEPLOYER
 cp MGMT-WEEU-DEP00-INFRASTRUCTURE/ MGMT-NOEU-DEP00-INFRASTRUCTURE/ -r
 cd MGMT-NOEU-DEP00-INFRASTRUCTURE
-cp ./config_files/MGMT-NOEU-DEP00-INFRASTRUCTURE.json .
+cp ~/souvenir-ui-addon/config_files/MGMT-NOEU-DEP00-INFRASTRUCTURE.json .
 
-rm ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/MGMT-NOEU-SAP_LIBRARY.json
-cp ./config_files/MGMT-NOEU-SAP_LIBRARY.json ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
+rm ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION/LIBRARY/MGMT-NOEU-SAP_LIBRARY/MGMT-NOEU-SAP_LIBRARY.json
+cp ~/souvenir-ui-addon/config_files/MGMT-NOEU-SAP_LIBRARY.json ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
 
-cd ~/Azure_SAP_Automated_Deployment/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
+cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION/LIBRARY/MGMT-NOEU-SAP_LIBRARY/
 ${DEPLOYMENT_REPO_PATH}deploy/scripts/install_library.sh -p MGMT-NOEU-SAP_LIBRARY.json -d ../../DEPLOYER/MGMT-NOEU-DEP00-INFRASTRUCTURE
