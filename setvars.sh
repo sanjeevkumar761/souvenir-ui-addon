7122cc32a3929c014bf14d93ced1f595f42fcca4
Sanjeev Kumar <sanjeku@microsoft.com>
2021-04-16 17:41:54 +0200

    Added library setup steps

--- setvars.sh
+++ setvars.sh
@@ -1,2 +1,2 @@
 export ARM_SUBSCRIPTION_ID=bf44c08e-5fa3-4f73-987c-3a51f2d22b00
-export DEPLOYMENT_REPO_PATH=./Azure_SAP_Automated_Deployment/sap-hana/
+export DEPLOYMENT_REPO_PATH=~/Azure_SAP_Automated_Deployment/sap-hana/
