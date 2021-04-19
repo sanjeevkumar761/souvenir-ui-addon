415c363f4a410a0ae0813c233387a714242d930a
Sanjeev Kumar <sanjeku@microsoft.com>
2021-04-19 20:24:46 +0200

    Updated steps for system

--- orchestrator.sh
+++ orchestrator.sh
@@ -7,9 +7,11 @@ chmod +x ./setvars.sh
 chmod +x download_hana_repo.sh
 ./download_hana_repo.sh
 
-chmod +x install_library.sh
-./install_library.sh
+chmod +x prepare_region.sh
+./prepare_region.sh
 
 chmod +x install_workload.sh
 #./install_workload.sh
 
+chmod +x install_system.sh
+#./install_system.sh
---
+++ remove_system.sh
@@ -0,0 +1,4 @@
+cd ~/Azure_SAP_Automated_Deployment/WORKSPACES/DEPLOYMENT-ORCHESTRATION
+cd SYSTEM
+cd DEV-NOEU-SAP01-X00
+${DEPLOYMENT_REPO_PATH}deploy/scripts/remover.sh -p DEV-NOEU-SAP01-X00.json -t sap_system
