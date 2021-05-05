4a7f45733b86134172eddc23caeb8a79a72489c9
Sanjeev Kumar <sanjeku@microsoft.com>
2021-05-05 11:28:03 +0200

    Updated to align with latest version of SAP HANA repo

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,4 +1,4 @@
-wget https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.7_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
 unzip terraform_0.14.7_linux_amd64.zip
 #wget https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_amd64.zip
 #unzip terraform_0.15.0_linux_amd64.zip
